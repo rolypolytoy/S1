b83165045d095d90c01d144e998d314fd31b7e39
Attack Orange <rishiitsharma@gmail.com>
2025-05-09 22:20:31 +0400

    push

--- Electron Column/sem.py
+++ Electron Column/sem.py
@@ -2,7 +2,7 @@ import numpy as np
 from picht import IonOpticsSystem, ElectrodeConfig
 import matplotlib.pyplot as plt
 
-system = IonOpticsSystem(nr=100, nz=600, axial_size=0.6, radial_size = 0.1) #all grid units are in mm.
+system = IonOpticsSystem(nr=100, nz=400, axial_size=0.4, radial_size = 0.1)
 
 
 wehnelt1 = ElectrodeConfig(
@@ -16,24 +16,24 @@ wehnelt1 = ElectrodeConfig(
 wehnelt2 = ElectrodeConfig(
     start=30,
     width=5,
-    ap_start=40,
-    ap_width=20,
+    ap_start=45,
+    ap_width=10,
     outer_diameter = 50,
     voltage=-5100
 )
 system.add_electrode(wehnelt1)
 system.add_electrode(wehnelt2)
 anode = ElectrodeConfig(
-    start=40,
+    start=50,
     width = 2,
-    ap_start=48,
-    ap_width=4,
+    ap_start=49,
+    ap_width=2,
     outer_diameter = 50,
     voltage=0
 )
 cathode = ElectrodeConfig(
-    start=22,
-    width = 2,
+    start=24,
+    width = 1,
     ap_start=50,
     ap_width=0,
     outer_diameter = 2,
@@ -41,36 +41,39 @@ cathode = ElectrodeConfig(
 )
 
 system.add_electrode(anode)
-system.add_einzel_lens(
-    position=80.0,
-    width=60.0,
-    aperture_center=50.0,
-    aperture_width=48.0,
-    outer_diameter=50.0,
-    focus_voltage=-7000
-)
+system.add_electrode(cathode)
+
 
 system.add_einzel_lens(
-    position=160.0,
-    width=60.0,
+    position= 70.0,
+    width=70.0,
     aperture_center=50.0,
     aperture_width=48.0,
     outer_diameter=50.0,
-    focus_voltage=-6500
+    focus_voltage=-7200
 )
+system.add_einzel_lens(
+    position= 142.0,
+    width=63.0,
+    aperture_center=50.0,
+    aperture_width=48.0,
+    outer_diameter=50.0,
+    focus_voltage=-10000
+)
+
+
 potential = system.solve_fields()
 
 trajectories = system.simulate_beam(
-    energy_eV= 10,  
+    energy_eV= 0.1,  
     start_z=0.025,
     r_range=(0.0499925, 0.0500075),
     angle_range=(-2, 2),
-    num_particles=6,
+    num_particles=100,
     simulation_time=1e-8
 )
 
 figure = system.visualize_system(
-    trajectories=trajectories,
-    r_limits = (0.049, 0.051))
+    trajectories=trajectories)
 
 plt.show()
